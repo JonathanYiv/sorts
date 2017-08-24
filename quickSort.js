22966003ec212fab5dadbc1e4d68818fcfdce18e
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-24 14:34:23 -0400

    quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,1 +1,24 @@
-// Coming soon...
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var partition = function(array, p, r) {
+    var q = p;
+    for (var j = p; j < r; j++) {
+        if (array[j] <= array[r]) {
+            swap(array, j, q++);
+        }
+    }
+    swap(array, r, q);
+    return q;
+};
+
+var quickSort = function(array, p, r) {
+    if(p < r) {
+        var pivot = partition(array, p, r);
+        quickSort(array, p, pivot - 1);
+        quickSort(array, pivot + 1, r);
+    }
+};
